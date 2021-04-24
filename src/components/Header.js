dc7ac4984d5e16aed7c4a7e3593fd27ef1e17326
Khai Phan Van <phan_van_khai123@yahoo.com>
2021-04-24 18:44:13 +0700

    SetupHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../css/Header.css"
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return (
@@ -49,7 +50,12 @@ function Header() {
                         Prime
                     </span>
                 </div>
-
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header__basketCount">
+                        0
+                    </span>
+                </div>
             </div>
         </div>
     );
